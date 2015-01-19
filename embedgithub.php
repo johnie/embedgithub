fc73ac725c337a82fb6b0b4e459adee19e2953c7
Johnie Hjelm <johnie.hjelm@symbio.com>
2015-01-19 22:45:35 +0100

    Trying to connect to Michelf Markdown

--- embedgithub.php
+++ embedgithub.php
@@ -38,7 +38,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Require Michelf Markdown
-require_once 'Michelf/Markdown.inc.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use \Michelf\Markdown;
 
 if ( ! class_exists( 'EmbedGithub' ) ) {
@@ -64,6 +64,48 @@ if ( ! class_exists( 'EmbedGithub' ) ) {
     protected $version = '0.0.1';
 
     /**
+     * Get github readme
+     *
+     * @access public
+     * @param array $atts
+     * @param string $content
+     * @return string
+     */
+    public function _eg_shortcode_readme( $atts, $html ) {
+
+      extract( shortcode_atts( array(
+        'repo' => 'johnie/embedgithub',
+        'trim' => 0
+      ), $atts ) );
+
+      $transient="embedgithub_" . $repo . "_" . $trim;
+      if ( false === ( $html = get_transient($transient) ) ) {
+        $url="https://api.github.com/repos/" . $repo . "/readme";
+
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_USERAGENT, 'WordPress' );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+        $data = curl_exec( $ch );
+        curl_close( $ch );
+
+        $json=json_decode( $data );
+        $markdown=base64_decode( $json->content );
+        if ( $trim>0 ) {
+          $markdown = implode("\n", array_slice(explode("\n", $markdown), $trim));
+        }
+
+        $html = Markdown::defaultTransform( $markdown );
+        set_transient( $transient, $html , 12 * HOUR_IN_SECONDS );
+      }
+
+      return $html;
+
+    }
+
+    /**
     * Initiate the plugin by setting the default values and assigning any
     * required actions and filters.
     *
@@ -71,7 +113,8 @@ if ( ! class_exists( 'EmbedGithub' ) ) {
     */
     function __construct() {
 
-      
+      // Add shortcode
+      add_shortcode( $this->tag, array( $this, '_eg_shortcode_readme' ) );
 
     }
 
