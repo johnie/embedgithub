9b7cbcdc4d789cf3dc64f1a945257cadef62389a
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-01-07 17:02:49 +0100

    Improve code style

--- embedgithub.php
+++ embedgithub.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 * Plugin Name: Embed Github
 * Plugin URI: https://github.com/johnie/embedgithub
@@ -29,7 +30,6 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-
 */
 
 // Exit if accessed directly.
@@ -41,89 +41,87 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once __DIR__ . '/vendor/autoload.php';
 use \Michelf\Markdown;
 
-if ( ! class_exists( 'EmbedGithub' ) ) {
+// If EmbedGitHub class exists don't create it.
+if ( class_exists( 'EmbedGitHub' ) ) {
+  return;
+}
 
-  class EmbedGithub {
+class EmbedGitHub {
 
-    /**
-     * Tag identifier used by file includes and selector attributes.
-     * @var string
-     */
-    protected $tag = 'embedgithub';
+  /**
+   * Tag identifier used by file includes and selector attributes.
+   *
+   * @var string
+   */
+  protected $tag = 'embedgithub';
 
-    /**
-     * User friendly name used to identify the plugin.
-     * @var string
-     */
-    protected $name = 'Embed Github';
+  /**
+   * User friendly name used to identify the plugin.
+   *
+   * @var string
+   */
+  protected $name = 'Embed Github';
 
-    /**
-     * Current version of the plugin.
-     * @var string
-     */
-    protected $version = '1.0.1';
+  /**
+   * Current version of the plugin.
+   *
+   * @var string
+   */
+  protected $version = '1.0.1';
 
-    /**
-     * Get github readme
-     *
-     * @param array $atts
-     * @param string $content
-     * @return string
-     */
-    public function _eg_shortcode_readme( $atts ) {
+  /**
+   * Get GitHub readme or file.
+   *
+   * @param  array $atts
+   *
+   * @return string
+   */
+  public function _eg_shortcode_readme( $atts ) {
+    $atts = shortcode_atts( array(
+      'repo' => 'johnie/embedgithub',
+      'file' => false,
+      'trim' => 0
+    ), $atts );
 
-      $atts = shortcode_atts( array(
-        'repo' => 'johnie/embedgithub',
-        'file' => false,
-        'trim' => 0
-      ), $atts );
-
-      $transient="embedgithub_" . $atts['repo'] . "_" . $atts['file'] . "_" . $atts['trim'];
-      if ( false === ( $html = get_transient( $transient ) ) ) {
-
-        if ( $atts['file'] ) {
-          $url="https://api.github.com/repos/" . $atts['repo'] . "/contents/" . $atts['file'];
-        } else {
-          $url="https://api.github.com/repos/" . $atts['repo'] . "/readme";
-        }
-
-        $ch = curl_init();
-        $timeout = 5;
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        curl_setopt( $ch, CURLOPT_USERAGENT, 'WordPress' );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-        curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
-        $data = curl_exec( $ch );
-        curl_close( $ch );
-
-        $json = json_decode( $data );
-        $markdown = base64_decode( $json->content );
-
-        if ( $atts['trim'] > 0 ) {
-          $markdown = implode( "\n", array_slice( explode( "\n", $markdown ), $atts['trim'] ) );
-        }
-
-        $html = Markdown::defaultTransform( $markdown );
-        set_transient( $transient, $html , 12 * HOUR_IN_SECONDS );
+    $transient = 'embedgithub_' . $atts['repo'] . '_' . $atts['file'] . '_' . $atts['trim'];
+    if ( false === ( $html = get_transient( $transient ) ) ) {
+      if ( $atts['file'] ) {
+        $url = 'https://api.github.com/repos/' . $atts['repo'] . '/contents/' . $atts['file'];
+      } else {
+        $url = 'https://api.github.com/repos/' . $atts['repo'] . '/readme';
       }
 
-      return $html;
+      $ch = curl_init();
+      $timeout = 5;
+      curl_setopt( $ch, CURLOPT_URL, $url );
+      curl_setopt( $ch, CURLOPT_USERAGENT, 'WordPress' );
+      curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+      curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+      $data = curl_exec( $ch );
+      curl_close( $ch );
 
+      $json = json_decode( $data );
+      $markdown = base64_decode( $json->content );
+
+      if ( $atts['trim'] > 0 ) {
+        $markdown = implode( "\n", array_slice( explode( "\n", $markdown ), $atts['trim'] ) );
+      }
+
+      $html = Markdown::defaultTransform( $markdown );
+      set_transient( $transient, $html , 12 * HOUR_IN_SECONDS );
     }
 
-    /**
-     * Initiate the plugin by setting the default values and assigning any
-     * required actions and filters.
-     */
-    function __construct() {
-
-      // Add shortcode
-      add_shortcode( $this->tag, array( $this, '_eg_shortcode_readme' ) );
-
-    }
+    return $html;
 
   }
 
-  new EmbedGithub();
-
+  /**
+   * Initiate the plugin by setting the default values and assigning any
+   * required actions and filters.
+   */
+  public function __construct() {
+    add_shortcode( $this->tag, array( $this, '_eg_shortcode_readme' ) );
+  }
 }
+
+new EmbedGitHub();
